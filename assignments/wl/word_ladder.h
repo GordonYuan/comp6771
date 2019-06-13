9f8fafd465aeb8d9caf45b38b45b8da018ff3e76
bofeiw <bofeiw@icloud.com>
2019-06-14 09:21:22 +1000

    add function headers

--- assignments/wl/word_ladder.h
+++ assignments/wl/word_ladder.h
@@ -1,6 +1,31 @@
 #ifndef ASSIGNMENTS_WL_WORD_LADDER_H_
 #define ASSIGNMENTS_WL_WORD_LADDER_H_
 
-// TODO(students): Add some stuff here
+#include <unordered_map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+ * return filtered set of words that is not as same length as the given word
+ * */
+set<string> filterDissimilarWords(set<string> words, string word);
+
+/*
+ * return the set of words that is one distance to the given word
+ * */
+set<string> getWordMap(set<string> words, string word);
+
+/*
+ * return a map of all words that map to the set of one distance words
+ * */
+unordered_map<string, set<string>> getWordMapAll();
+
+/*
+ * return the word ladder if any, null otherwise
+ * */
+vector<string> computeLadder(set<string> words, string from, string to);
 
 #endif  // ASSIGNMENTS_WL_WORD_LADDER_H_
