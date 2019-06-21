9ab8baf52e4b75a9be6fb4d6abdb8861379f3d1a
bofeiw <bofeiw@icloud.com>
2019-06-22 02:15:13 +1000

    add test

--- assignments/wl/word_ladder_test.cpp
+++ assignments/wl/word_ladder_test.cpp
@@ -8,85 +8,36 @@
 
 */
 
-#include "lexicon.h"
+#include <string>
+#include <unordered_set>
+#include <vector>
+
 #include "assignments/wl/word_ladder.h"
 #include "catch.h"
+#include "lexicon.h"
 
-SCENARIO("Filter a set") {
-    auto words = GetLexicon("assignments/wl/words.txt");
-    WHEN("filter words to the same length as word 'abc'") {
-        auto filteredWords = filterDissimilarWords(words, "abc");
-        THEN("all filtered words should has length 3") {
-            for (const string& word : filteredWords) {
-                REQUIRE(word.size() == 3);
-            }
-        }
+SCENARIO("Find ladders", "[computeLadder]") {
+  GIVEN("3 words which forms a ladder") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders =
+          computeLadder(lexicon, "con", "cat");
+      THEN("one solution") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"}};
+        REQUIRE(ladders == solution);
+      }
     }
-}
+  }
 
-SCENARIO("getWordMap") {
-    unordered_set<string> words;
-    words.insert("abd");    // only these words are ladder
-    words.insert("aec");    // only these words are ladder
-    words.insert("fbc");    // only these words are ladder
-    words.insert("bec");    // but not this
-
-    string word = "abc";
-    WHEN("call getWordMap") {
-        auto ladders = getWordMap(words, word);
-        THEN("only abd, aec, fbc should be ladder, but not bec") {
-            for (const auto& it : ladders) {
-                std::cout<<it;
-            }
-            REQUIRE(ladders.size() == 3);
-            REQUIRE(ladders.find("abd") != ladders.end());
-            REQUIRE(ladders.find("aec") != ladders.end());
-            REQUIRE(ladders.find("fbc") != ladders.end());
-            REQUIRE(ladders.find("bec") == ladders.end());
-        }
+  GIVEN("3 words which forms a ladder") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders =
+          computeLadder(lexicon, "con", "cat");
+      THEN("one solution") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"}};
+        REQUIRE(ladders == solution);
+      }
     }
-}
-
-SCENARIO("getWordMapAll") {
-    unordered_set<string> words;
-    words.insert("abd");
-    words.insert("aec");
-    words.insert("fbc");
-    words.insert("bec");
-
-    string word = "abc";
-    WHEN("call getWordMapAll") {
-        auto map = getWordMapAll(words);
-
-        THEN("get a map of four elements") {
-            REQUIRE(map.size() == 4);
-        }
-
-        AND_WHEN("get the mapped values of abd") {
-            auto abd = map["abd"];
-
-            REQUIRE(abd.empty());
-        }
-
-        AND_WHEN("get the mapped values of aec") {
-            auto aec = map["aec"];
-
-            REQUIRE(aec.size() == 1);
-            REQUIRE(aec.find("bec") != aec.end());
-        }
-
-        AND_WHEN("get the mapped values of fbc") {
-            auto fbc = map["fbc"];
-
-            REQUIRE(fbc.empty());
-        }
-
-        AND_WHEN("get the mapped values of bec") {
-            auto bec = map["bec"];
-
-            REQUIRE(bec.size() == 1);
-            REQUIRE(bec.find("aec") != bec.end());
-        }
-    }
-
+  }
 }
