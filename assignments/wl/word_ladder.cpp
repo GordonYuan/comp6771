30cb94afb2511083c2d98c27dc403043a183b66b
bofeiw <bofeiw@icloud.com>
2019-06-21 17:29:29 +1000

    time is within 10x ref for 99% cases

--- assignments/wl/word_ladder.cpp
+++ assignments/wl/word_ladder.cpp
@@ -20,23 +20,6 @@ bool neighbour(const string &word1, const string &word2) {
     return differenceOccurred;
 }
 
-// fucking slow!
-//bool neighbour(const string &word1, const string &word2, map<pair<string, string>, bool> &cacheNeighbour) {
-//    auto result = cacheNeighbour.find(make_pair(word1, word2));
-//    if (result == cacheNeighbour.end()) {
-//        result = cacheNeighbour.find(make_pair(word2, word1));
-//    }
-//    if (result != cacheNeighbour.end()) {
-//        // cached
-//        return (*result).second;
-//    } else {
-//        // not cached, calculate and cache
-//        bool isNeighbour = neighbourCalculate(word1, word2);
-//        cacheNeighbour[make_pair(word2, word1)] = isNeighbour;
-//        return isNeighbour;
-//    }
-//}
-
 void filterDissimilarWords(unordered_set<string> &words, const string &word) {
     auto wordLength = word.size();
 
@@ -245,7 +228,7 @@ unsigned short calculateMinHops(unordered_set<string> &words, const string &from
 vector<unordered_set<string>> getHops(unordered_set<string> &words, const string &from, const string &to) {
     unordered_map<string, unsigned short> depths;
     queue<string> ladderQueue;
-    unordered_set<string> newWords;
+//    unordered_set<string> newWords;
 
     depths.insert({from, 1});
     ladderQueue.push(from);
@@ -256,6 +239,7 @@ vector<unordered_set<string>> getHops(unordered_set<string> &words, const string
         ladderQueue.pop();
         if (curr == to) {
             minHops = depths[curr];
+//            break;
         } else {
             unsigned short nextDepth = depths[curr] + 1;
             auto it = words.begin();
@@ -264,7 +248,7 @@ vector<unordered_set<string>> getHops(unordered_set<string> &words, const string
                 if (neighbour(word, curr)) {
                     ladderQueue.push(word);
                     depths.insert({word, nextDepth});
-                    newWords.insert(word);
+//                    newWords.insert(word);
                     it = words.erase(it);
                 } else {
                     ++it;
@@ -273,16 +257,13 @@ vector<unordered_set<string>> getHops(unordered_set<string> &words, const string
         }
     }
 
-    cerr << "depths done" << endl;
-
     vector<unordered_set<string>> hops;
+
+    if (! minHops) {
+        return hops;
+    }
     for (unsigned short i = 0; i < minHops; ++i) {
         hops.emplace_back(unordered_set<string>{});
-    }
-
-    cerr << "size: " << hops.size() << endl;
-    if (hops.empty()) {
-        return hops;
     }
 
     // convert to hops
@@ -290,20 +271,10 @@ vector<unordered_set<string>> getHops(unordered_set<string> &words, const string
         const unsigned short depth = entry.second;
         if (depth < minHops) {
             const string &word = entry.first;
-//            cerr << "[" << word << "." << depth << "]" << endl;
             hops[depth - 1].insert(word);
         }
     }
     hops[minHops - 1].insert(to);
-
-    cerr << "convet done" << endl;
-    for (int i = 0; i < minHops; ++i) {
-        cerr << "D " << i << ": ";
-        for (auto word: hops[i]) {
-            cerr << word << " ";
-        }
-        cerr << endl;
-    }
 
     // filter words cant be reversely achieved from 'to'
     for (reverse_iterator fromHops = hops.rbegin(); (fromHops + 1) != hops.rend(); ++fromHops) {
@@ -327,58 +298,14 @@ vector<unordered_set<string>> getHops(unordered_set<string> &words, const string
         }
     }
 
-    cerr << "convet done" << endl;
-    for (int i = 0; i < minHops; ++i) {
-        cerr << "D " << i << ": ";
-        for (auto word: hops[i]) {
-            cerr << word << " ";
-        }
-        cerr << endl;
-    }
-
-    cerr << "exiting hops" << endl;
     return hops;
-}
-
-void computeLadderRecursive(unordered_set<string> &words, const string &to, vector<string> &lStack,
-                            vector<vector<string>> &ladders, const unsigned short &minHops) {
-    const string curr = lStack.back();
-//    for (auto word: lStack) {
-//        cerr << word << " ";
-//    }
-//    cerr << ": " << lStack.size() <<  endl;
-    if (curr == to) {
-//        cerr << "found " << endl;
-        ladders.push_back(lStack);
-//        lStack.pop_back();
-    } else if (lStack.size() < minHops) {
-        int i = 0;
-        for (const string &word: words) {
-            ++i;
-//            if (curr == "con") {
-//                cerr << ":" << word;
-//            }
-            if (find(lStack.begin(), lStack.end(), word) == lStack.end() && neighbour(curr, word)) {
-//                if (curr == "con") {
-//                    cerr << "::" << word << endl;
-//                }
-//                cerr << "\t" << word << endl;
-                lStack.push_back(word);
-                computeLadderRecursive(words, to, lStack, ladders, minHops);
-                lStack.pop_back();
-            }
-        }
-    }
 }
 
 vector<vector<string>> computeLadder(unordered_set<string> &words, const string &from, const string &to) {
     vector<vector<string>> ladders;
     filterDissimilarWords(words, from);
 
-//    cerr << "size: " << words.size() << endl;
-//    unsigned short minHops = calculateMinHops(words, from, to);
     auto hops = getHops(words, from, to);
-    cerr << "hops end" << endl;
     if (hops.empty()) {
         return ladders;
     }
@@ -390,11 +317,8 @@ vector<vector<string>> computeLadder(unordered_set<string> &words, const string 
         }
     }
 
-
-
     // construct ladders
     auto wordMap = getWordMapAll(newWords);
-
 
     // convert hops to one-to-many maps
     unordered_map<string, unordered_set<string>> ladderMap;
@@ -442,58 +366,11 @@ vector<vector<string>> computeLadder(unordered_set<string> &words, const string 
             stackDFS.push(word);
         }
     }
-//    cerr << "min : " << minHops << endl;
-//    cerr << "size: " << words.size() << endl;
-
-//    for (auto word: words) {
-//        cerr << word << endl;
-//    }
-
-//    vector<string> lStackTemp{from};
-//    computeLadderRecursive(words, to, lStackTemp, ladders, minHops);
-
-//    return computeLadderOld(words, from, to);
-
-
-    // construct ladders
-//    stack<string> stackDFS;
-//    vector<string> ladderStack;
-//    stackDFS.push(from);
-//    bool ladderStackNeedFix = false;
-//    while (!stackDFS.empty()) {
-//        string next = stackDFS.top();
-//        if (ladderStackNeedFix) {
-//            // after a ladder is found, re-construct ladder stack
-//            while (!ladderStack.empty()) {
-//                string back = ladderStack.back();
-//                if (neighbour(back, next)) {
-//                    break;
-//                } else {
-//                    ladderStack.pop_back();
-//                }
-//            }
-//        }
-//        ladderStack.push_back(next);
-//        if (next == to) {
-//            // a ladder is found
-//            ladders.push_back(ladderStack);
-//            ladderStackNeedFix = true;
-//        }
-//        stackDFS.pop();
-//        for (const string &word: newWords) {
-//            if (neighbour(word, next))
-//                stackDFS.push(word);
-//        }
-//    }
     return ladders;
 }
 
 void sortLadders(vector<vector<string>> &ladders) {
-    auto ladderGreater = [](const vector<string> &ladder1, const vector<string> &ladder2) -> bool {
-        return accumulate(ladder1.cbegin(), ladder1.cend(), string()) <
-               accumulate(ladder2.cbegin(), ladder2.cend(), string());
-    };
-    sort(ladders.begin(), ladders.end(), ladderGreater);
+    sort(ladders.begin(), ladders.end());
 }
 
 void printLadders(const vector<vector<string>> &ladders) {
