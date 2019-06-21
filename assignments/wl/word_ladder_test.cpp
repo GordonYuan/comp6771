cf3caf95dd2b6d4171188bbdb5d8dc980e83efe0
bofeiw <bofeiw@icloud.com>
2019-06-22 03:39:38 +1000

    add tests

--- assignments/wl/word_ladder_test.cpp
+++ assignments/wl/word_ladder_test.cpp
@@ -28,11 +28,83 @@ SCENARIO("Find ladders", "[computeLadder]") {
     }
   }
 
-  GIVEN("3 words which forms a ladder") {
-    std::unordered_set<std::string> lexicon{"con", "can", "cat"};
+  GIVEN("3 words which does not forms a ladder") {
+    std::unordered_set<std::string> lexicon{"con", "abc", "cat"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("no solution") {
+        std::vector<std::vector<std::string>> solution{};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("4 words which forms 2 ladders") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat", "cot"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("2 solutions") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"},
+                                                       {"con", "cot", "cat"}};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("4 words which forms 1 ladder") {
+    std::unordered_set<std::string> lexicon{"con", "can", "abc", "cat"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("1 solution") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"}};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("many words which forms 2 ladders") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat", "cot", "abc",
+                                            "aba", "aaa", "bab", "cda"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("2 solutions") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"},
+                                                       {"con", "cot", "cat"}};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("4 words which forms a ladder, including one with different length") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat", "concancat"};
     WHEN("search for ladders") {
       std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
       THEN("one solution") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"}};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("many words which forms 2 ladders, including some with different length") {
+    std::unordered_set<std::string> lexicon{
+        "con", "can", "cat", "cot", "abc", "concancat", "canconcat",
+        "bab", "cda", "c",   "a",   "b",   "ba",        "asdgausgboasduighsaipghasi"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("2 solutions") {
+        std::vector<std::vector<std::string>> solution{{"con", "can", "cat"},
+                                                       {"con", "cot", "cat"}};
+        REQUIRE(ladders == solution);
+      }
+    }
+  }
+
+  GIVEN("words that contains paths including short path and long paths") {
+    std::unordered_set<std::string> lexicon{"con", "can", "cat", "don", "dan", "dat"};
+    WHEN("search for ladders") {
+      std::vector<std::vector<std::string>> ladders = computeLadder(lexicon, "con", "cat");
+      THEN("only shortest path is returned") {
         std::vector<std::vector<std::string>> solution{{"con", "can", "cat"}};
         REQUIRE(ladders == solution);
       }
