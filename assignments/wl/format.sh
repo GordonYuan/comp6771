37df8a15f145e5ea398cd05bf5c9a591ba371d58
bofeiw <bofeiw@icloud.com>
2019-06-22 15:23:59 +1000

    remove duplicate

--- assignments/wl/format.sh
+++ assignments/wl/format.sh
@@ -4,7 +4,7 @@ cd ../../
 
 ASSIGNMENT="assignments/wl/"
 
-for FILE in "word_ladder_test.cpp" "main.cpp" "word_ladder.h" "word_ladder_test.cpp" "word_ladder.cpp"
+for FILE in "main.cpp" "word_ladder.h" "word_ladder_test.cpp" "word_ladder.cpp"
 do
     FULL_PATH=${ASSIGNMENT}${FILE}
     clang-format -i ${FULL_PATH} && python2 cpplint.py ${FULL_PATH}
